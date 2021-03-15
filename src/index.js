4f0e836c45309459edd0a225f6318f912efe853b
AlmazerX <79789378+AlmazerX@users.noreply.github.com>
2021-03-16 00:36:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   bracketsConfig = [];
+
+    let open = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    };
+
+    let closed = {
+        '}': true,
+        ']': true,
+        ')': true
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        let char = str[i];
+
+        if (open[char]) {
+            bracketsConfig.push(char);
+        } else if (closed[char]) {
+            if (open[bracketsConfig.pop()] !== char) return false;
+        }
+    }
+    return bracketsConfig.length === 0;
 }
